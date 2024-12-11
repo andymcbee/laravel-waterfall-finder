843aaf9c6961a4d97d933794c9dbd4d2933ef522
Andrew <armcburn@gmail.com>
2024-12-11 15:06:01 -0500

    add waterfall model

--- server/app/Models/Waterfall.php
+++ server/app/Models/Waterfall.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Waterfall extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name', 'height', 'province', 'location'];
+
+    public $timestamps = true;
+
+    /**
+     * Define the casts for the properties.
+     */
+    protected $casts = [
+        'location' => 'array', // To handle the geometry data
+    ];
 }
